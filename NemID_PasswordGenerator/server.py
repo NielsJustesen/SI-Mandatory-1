b81cc1a765b5235d45af1e8db6dabc616845d195
ludvig <ludvigrandersen@gmail.com>
2020-10-04 14:54:43 +0200

    added server.py files in microservices

---
+++ NemID_PasswordGenerator/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/generate-password-nemID')
+def generate():
+	data = request.get_json()
+	first_two = data["nemId"][:2]
+	last_two = data["cpr"][-2:]
+	return {"nemIdPassword": str(first_two) + str(last_two)}, 200
+
---
+++ NemID_UserGenerator/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+import random
+app = Flask(__name__)
+
+@app.route('/generate-nemID')
+def generate():
+	data = request.get_json()
+	last_four = data["cpr"][-4:]
+	first_five = random.randint(10000,99999)
+	return {"nemId":str(first_five) + last_four}, 201
+
