e5e5a687b17304892c4d8aa461cd6b24359eb940
NielsJustesen <niels449@hotmail.com>
2020-10-05 09:45:47 +0200

    generating CPR in legacy, trying to write multiple persons to xml doc..  not yet working

--- Main_System/LGC.py
+++ Main_System/LGC.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import random
+import xml.etree.ElementTree as ET
+
+
+# header = ET.Element('?xml version="1.0"?')
+
+# tree = ET.ElementTree(header)
+# tree.write("filename.xml")
 
 df = pd.read_csv("people.csv")
 
 #FirstName,LastName,Email,DateOfBirth,Phone,Address,Country
-firstNames, lastNames, emails, datesOfBirth, phoneNumbers,address,countries = "","","","","","",""
-
-
-
 
 for index, row in df.iterrows():
     firstName = row["FirstName"]
@@ -16,10 +20,19 @@ for index, row in df.iterrows():
     phone = row["Phone"]
     address = row["Address"]
     country = row["Country"]
-    print(firstName)
-    print(lastname)
-    print(email)
-    print(dateOfBirth)
-    print(phone)
-    print(address)
-    print(country)
+    
+    lastFour = random.randint(1000,9999)
+    
+    cpr = dateOfBirth[:2] + dateOfBirth[3:5] + dateOfBirth [6:] + str(lastFour)
+    print(cpr)
+      
+    person = ET.Element("Person")   
+    ET.SubElement(person, "FirstName").text = firstName
+    ET.SubElement(person, "LastName").text = lastname
+    ET.SubElement(person, "CprNumber").text = cpr
+    ET.SubElement(person, "Email").text = email
+    
+
+tree = ET.ElementTree(person)
+# ET.append(person)
+tree.write("filename.xml")
