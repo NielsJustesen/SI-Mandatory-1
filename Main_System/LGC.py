35addebe1189ac82b2990c4147cbb94ed0e2d07d
ludvig <ludvigrandersen@gmail.com>
2020-10-05 13:32:22 +0200

    LGC file changes

--- Main_System/LGC.py
+++ Main_System/LGC.py
@@ -1,20 +1,16 @@
 import pandas as pd
 import random
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as gfg
+import requests
 
-
-# header = ET.Element('?xml version="1.0"?')
-
-# tree = ET.ElementTree(header)
-# tree.write("filename.xml")
-
+tree = gfg.ElementTree()
 df = pd.read_csv("people.csv")
-
+root = gfg.Element(tree.getroot()) 
 #FirstName,LastName,Email,DateOfBirth,Phone,Address,Country
 
 for index, row in df.iterrows():
     firstName = row["FirstName"]
-    lastname = row["LastName"]
+    lastName = row["LastName"]
     email = row["Email"]
     dateOfBirth = row["DateOfBirth"]
     phone = row["Phone"]
@@ -24,15 +20,15 @@ for index, row in df.iterrows():
     lastFour = random.randint(1000,9999)
     
     cpr = dateOfBirth[:2] + dateOfBirth[3:5] + dateOfBirth [6:] + str(lastFour)
-    print(cpr)
-      
-    person = ET.Element("Person")   
-    ET.SubElement(person, "FirstName").text = firstName
-    ET.SubElement(person, "LastName").text = lastname
-    ET.SubElement(person, "CprNumber").text = cpr
-    ET.SubElement(person, "Email").text = email
-    
 
-tree = ET.ElementTree(person)
-# ET.append(person)
+    m1 = gfg.Element("person") 
+    root.append (m1) 
+
+    gfg.SubElement(m1, "FirstName").text = firstName
+    gfg.SubElement(m1, "LastName").text = lastName
+    gfg.SubElement(m1, "CprNumber").text = cpr
+    gfg.SubElement(m1, "Email").text = email
+    
+    
+tree = gfg.ElementTree(root)
 tree.write("filename.xml")
